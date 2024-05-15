96acf2aaef601f4eac937db331bf913f7af4bc83
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2024-05-15 10:38:48 +0530

    Update MessageTemplate.jsx

--- src/Components/MessageTemplate.jsx
+++ src/Components/MessageTemplate.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const MessageTemplate = ({ username, createdAt, message,darkMode }) => {
   const limitedUsername = username.length> 10 ? username.slice(0, 10) + '...' : username;
+  const iconName = limitedUsername.slice(0,1).toUpperCase();
   return (
     <div className="message">
         {/* <p>
@@ -12,11 +13,16 @@ const MessageTemplate = ({ username, createdAt, message,darkMode }) => {
 
 
         <div className="chat chat-start">
-          <div className="chat-image avatar">
+          {/* <div className="chat-image avatar">
             <div className="w-10 rounded-full">
               <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
-          </div>
+          </div> */}
+          <div className="avatar placeholder chat-image avatar">
+            <div className="bg-neutral text-neutral-content rounded-full w-12 ring ring-accent ring-offset-base-100 ring-offset-2">
+              <span>{iconName}</span>
+            </div>
+          </div> 
           <div className={`${darkMode ? 'text-white' : ' text-black'}  chat-header`} >
           {limitedUsername}
           <span className="message__meta" style={{padding: '0px 5px'}}>{createdAt}</span>
